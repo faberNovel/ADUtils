c357dc1d81a720d9ad856444005cff3c081f276a
Benjamin Lavialle <benjamin.lavialle@applidium.com>
2016-11-21 14:52:04 +0100

    Add view and VC categories
    Change-Id: Ia93e291fdb8bc01d4f385437de36da8174dcdd13

---
+++ Modules/ADUtils/UIViewController+ChildInsertion.swift
@@ -0,0 +1,38 @@
+//
+//  UIViewController+ChildInsertion.swift
+//  BabolatPulse
+//
+//  Created by Benjamin Lavialle on 06/09/16.
+//
+//
+
+import Foundation
+
+extension UIViewController {
+
+    /**
+     * Inset viewController as a child, inserting its view inside subview
+     * subview has to be a subView of view
+     * viewController's view is inserted wihtout margins
+     */
+
+    func ad_insertChild(viewController: UIViewController, inSubview subview: UIView) {
+        guard subview.isDescendantOfView(view) else { return }
+        addChildViewController(viewController)
+        let viewControllerView: UIView = viewController.view
+        viewControllerView.translatesAutoresizingMaskIntoConstraints = false
+        subview.ad_addSubview(viewControllerView, withMargins: UIEdgeMarginsZero)
+        viewController.didMoveToParentViewController(self)
+    }
+
+    /**
+     * Remove viewController from parent view controller if self is its parent
+     */
+
+    func ad_removeChild(viewController: UIViewController) {
+        guard viewController.parentViewController == self else { return }
+        viewController.willMoveToParentViewController(nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParentViewController()
+    }
+}
