7050a074dfe19c1981b7e798404028f2ddb13956
Roland Borgese <roland.borgese@applidium.com>
2017-06-05 15:01:57 +0200

    Add parameters owner and bundle in ad_fromNib method
    Change-Id: I87f533f3f6a131e4f1d0a8292b0757e8a4843ad3

--- Modules/ADUtils_objc/UIView+NibLoader_objc.swift
+++ Modules/ADUtils_objc/UIView+NibLoader_objc.swift
@@ -11,10 +11,21 @@ import Foundation
 extension UIView {
 
     @objc public class func adobjc_fromNib() -> Self? {
-        return adobjc_fromNib(nil)
+        return ad_fromNib()
     }
 
     @objc public class func adobjc_fromNib(_ nibNameOrNil: String?) -> Self? {
         return ad_fromNib(nibNameOrNil)
     }
+
+    @objc public class func adobjc_fromNib(_ nibNameOrNil: String?, owner: AnyObject?) -> Self? {
+        return ad_fromNib(nibNameOrNil, owner: owner)
+    }
+
+    @objc public class func adobjc_fromNib(_ nibNameOrNil: String?,
+                                           owner: AnyObject?,
+                                           bundle: Bundle) -> Self? {
+        return ad_fromNib(nibNameOrNil, owner: owner, bundle: bundle)
+    }
+
 }
