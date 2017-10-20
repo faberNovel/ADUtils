97f314e9ddc7804771b120bc5e58c31b7a2b56bf
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2017-10-20 15:09:37 +0200

    Add font description
    Change-Id: I2260381865a4954e09fae9cf21d786dc97409f2d

---
+++ Modules/ADUtils/FontDescription.swift
@@ -0,0 +1,43 @@
+//
+//  FontDescription.swift
+//  ADUtils
+//
+//  Created by Benjamin Lavialle on 20/10/2017.
+//
+
+import Foundation
+
+private typealias FontDescriptionDictionary = [UIFontTextStyle.RawValue: FontStyleDescription]
+
+struct FontDescription {
+
+    private let dictionary: FontDescriptionDictionary
+
+    //MARK: - FontDescription
+
+    init(fontName: String) throws {
+        guard let url = Bundle.main.url(forResource: fontName, withExtension: "plist") else {
+            throw FontDescriptionError.plistMissing
+        }
+        let data = try Data(contentsOf: url)
+        let decoder = PropertyListDecoder()
+        dictionary = try decoder.decode(FontDescriptionDictionary.self, from: data)
+    }
+
+    func fontStyleDescription(for fontTextStyle: UIFontTextStyle) throws -> FontStyleDescription {
+        guard let fontStyleDescription = dictionary[fontTextStyle.rawValue] else {
+            throw FontDescriptionError.styleForFontUnavailable
+        }
+        return fontStyleDescription
+    }
+}
+
+struct FontStyleDescription: Decodable {
+    let size: CGFloat
+    let name: String
+}
+
+enum FontDescriptionError: Error {
+    case styleForFontUnavailable
+    case plistMissing
+}
