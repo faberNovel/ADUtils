5e6d94247f9a325d237c56e3294b6a88feb449bc
Benjamin Lavialle <benjamin.lavialle@fabernovel.com>
2018-07-20 16:17:45 +0200

    Add comment on NibLoadable
    Change-Id: I3c4eddef39d11e759cb51cd9a2115c17ea54e36f

--- Modules/ADUtils/UIView+NibLoader.swift
+++ Modules/ADUtils/UIView+NibLoader.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+/// NibLoadable is a semantic protocol defining an object instanciatable from a nib
 public protocol NibLoadable {}
 
 extension UIView: NibLoadable {}
@@ -15,6 +16,13 @@ extension UIView: NibLoadable {}
 // http://stackoverflow.com/a/26326006/900937
 extension NibLoadable where Self: NibLoadable {
 
+    /**
+     Instanciate a Self object from a nib
+     - parameter nibNameOrNil: A custom nib name, if nil Self name is used as nibs are generally demangled, default is nil
+     - parameter owner: The nib owner, default is nil
+     - parameter bundle: The nib bundle, default is main bundle
+     - note: Crashes if unable to instanciate the objet, meaning if there is no matching nib
+     */
     public static func ad_fromNib(_ nibNameOrNil: String? = nil,
                                   owner: AnyObject? = nil,
                                   bundle: Bundle = Bundle.main) -> Self {
