d1d56a098f5da8170c52e8205789bba2c4e00508
Pierre Felgines <pierre.felgines@fabernovel.com>
2018-09-21 13:50:23 +0200

    Refactor test
    Change-Id: If755be123e03b5870dd50f72bf87ad00acdaea08

--- ADUtilsTests/ViewLayoutTest.swift
+++ ADUtilsTests/ViewLayoutTest.swift
@@ -17,69 +17,46 @@ class ViewLayout: QuickSpec {
 
         let veryLongText = "Lorem sizzle pimpin' sit amizzle, yippiyo adipiscing izzle. Nullizzle bling bling velit, away ghetto, suscipit dang, gravida vel, ass. Pellentesque eget tortor. Bow wow wow erizzle. Own yo' uhuh ... yih! sizzle dapibizzle turpis tempus shut the shizzle up. Maurizzle fo shizzle my nizzle crunk et turpizzle. Phat in tortizzle. Fizzle eleifend rhoncizzle i'm in the shizzle. In shut the shizzle up habitasse platea dictumst. Sheezy dapibizzle. Fo tellizzle urna, pretizzle eu, stuff shut the shizzle up, you son of a bizzle boofron, nunc. Fo suscipizzle. Integer sempizzle shizzle my nizzle crocodizzle ass purus"
 
-        describe("layout regular view") {
+        let width: CGFloat = 100
+        let frame = CGRect(x: 0.0, y: 0.0, width: 320.0, height: 100.0)
 
-            let width: CGFloat = 100
-            var view: UIView!
-
-            beforeEach {
-                view = UIView(
-                    frame: CGRect(x: 0.0, y: 0.0, width: 320.0, height: 100.0)
-                )
-                let label = UILabel()
-                label.numberOfLines = 0
-                label.text = veryLongText
-                view.addSubview(label)
-                label.ad_pinToSuperview()
-            }
-
-            it("should compute height") {
-                let height = view.ad_preferredLayoutHeight(fittingWidth: width)
-                expect(height).to(equal(CGFloat(1218.0)))
-            }
+        let addLabelInView = { (view: UIView) in
+            let label = UILabel()
+            label.numberOfLines = 0
+            label.text = veryLongText
+            view.addSubview(label)
+            label.ad_pinToSuperview()
         }
 
-        describe("layout UICollectionViewCell") {
-
-            let width: CGFloat = 100
-            var view: UICollectionViewCell!
-
-            beforeEach {
-                view = UICollectionViewCell(
-                    frame: CGRect(x: 0.0, y: 0.0, width: 320.0, height: 100.0)
-                )
-                let label = UILabel()
-                label.numberOfLines = 0
-                label.text = veryLongText
-                view.contentView.addSubview(label)
-                label.ad_pinToSuperview()
-            }
-
-            it("should compute height") {
-                let height = view.ad_preferredCellLayoutHeight(fittingWidth: width)
-                expect(height).to(equal(CGFloat(1218.0)))
-            }
+        it("should layout regular view") {
+            // Given
+            let view = UIView(frame: frame)
+            addLabelInView(view)
+            // When
+            let height = view.ad_preferredLayoutHeight(fittingWidth: width)
+            // Then
+            expect(height).to(equal(CGFloat(1218.0)))
         }
 
-        describe("layout UITableViewCell") {
+        it("should layout UICollectionViewCell") {
+            // Given
+            let cell = UICollectionViewCell(frame: frame)
+            addLabelInView(cell.contentView)
+            // When
+            let height = cell.ad_preferredCellLayoutHeight(fittingWidth: width)
+            // Then
+            expect(height).to(equal(CGFloat(1218.0)))
+        }
 
-            let width: CGFloat = 100
-            var view: UITableViewCell!
-
-            beforeEach {
-                view = UITableViewCell(style: .default, reuseIdentifier: nil)
-                view.frame = CGRect(x: 0.0, y: 0.0, width: 320.0, height: 100.0)
-                let label = UILabel()
-                label.numberOfLines = 0
-                label.text = veryLongText
-                view.contentView.addSubview(label)
-                label.ad_pinToSuperview()
-            }
-
-            it("should compute height") {
-                let height = view.ad_preferredCellLayoutHeight(fittingWidth: width)
-                expect(height).to(equal(CGFloat(1218.0)))
-            }
+        it("should layout UITableViewCell") {
+            // Given
+            let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+            cell.frame = frame
+            addLabelInView(cell.contentView)
+            // When
+            let height = cell.ad_preferredCellLayoutHeight(fittingWidth: width)
+            // Then
+            expect(height).to(equal(CGFloat(1218.0)))
         }
     }
 }
