fff8e5d7a797f3028fd5209b9a3721bf496b6b56
Pierre Felgines <pierre.felgines@fabernovel.com>
2018-09-24 16:55:44 +0200

    Create ad_groupedBy method
    Change-Id: I5ec6003052fcebefca3d4f28aef6ae6609f055c5

---
+++ ADUtilsTests/SequenceGroupedByTests.swift
@@ -0,0 +1,41 @@
+//
+//  SequenceGroupedByTests.swift
+//  ADUtilsTests
+//
+//  Created by Pierre Felgines on 24/09/2018.
+//
+
+import Foundation
+import Quick
+import Nimble
+import ADUtils
+
+private struct Person {
+    let name: String
+    let age: Int
+}
+
+class SequenceGroupedByTests: QuickSpec {
+
+    override func spec() {
+
+        it("should group the sequence") {
+            let persons = [
+                Person(name: "John", age: 10),
+                Person(name: "Brian", age: 20),
+                Person(name: "Peter", age: 30),
+                Person(name: "Bob", age: 10),
+            ]
+            let results = persons.ad_groupedBy { $0.age }
+            let nameResults = results.mapValues { persons in
+                persons.map { $0.name }
+            }
+            let expectedResults: [Int: [String]] = [
+                10: ["John", "Bob"],
+                20: ["Brian"],
+                30: ["Peter"]
+            ]
+            expect(nameResults).to(equal(expectedResults))
+        }
+    }
+}
