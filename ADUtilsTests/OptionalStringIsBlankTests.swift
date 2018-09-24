07fbf3b27e9750f1361422e968912c1f0ebc3707
Pierre Felgines <pierre.felgines@fabernovel.com>
2018-09-24 16:55:09 +0200

    Create ad_isBlank method
    Change-Id: I06b1b776d626ef6759db65e302acfe9c1f105a1e

---
+++ ADUtilsTests/OptionalStringIsBlankTests.swift
@@ -0,0 +1,35 @@
+//
+//  OptionalStringIsBlankTests.swift
+//  ADUtilsTests
+//
+//  Created by Pierre Felgines on 24/09/2018.
+//
+
+import Foundation
+import Quick
+import Nimble
+import ADUtils
+
+class OptionalStringIsBlankTests: QuickSpec {
+
+    override func spec() {
+
+        it("should be blank") {
+            // Given
+            let emptyString: String? = ""
+            let nilString: String? = nil
+
+            // Then
+            expect(emptyString.ad_isBlank).to(beTrue())
+            expect(nilString.ad_isBlank).to(beTrue())
+        }
+
+        it("should not be blank") {
+            // Given
+            let string: String? = "abc"
+
+            // Then
+            expect(string.ad_isBlank).to(beFalse())
+        }
+    }
+}
---
+++ Modules/ADUtils/Optional+IsBlank.swift
@@ -0,0 +1,18 @@
+//
+//  Optional+IsBlank.swift
+//  ADUtils
+//
+//  Created by Pierre Felgines on 24/09/2018.
+//
+
+import Foundation
+
+public extension Optional where Wrapped == String {
+
+    /**
+     Returns true if the string is empty or nil
+     */
+    public var ad_isBlank: Bool {
+        return self?.isEmpty ?? true
+    }
+}
