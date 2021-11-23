489e159f0ba3c7b85ad81bb008bac708655ea532
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2021-11-23 14:04:40 +0100

    Test UIDirectionalRectEdge+Utilities
    Change-Id: Id4e3b2d42989ec1f0472119fc34ed07cb26d5d30

---
+++ ADUtilsTests/UIDirectionalRectEdgeTest.swift
@@ -0,0 +1,47 @@
+//
+//  UIDirectionalRectEdgeTest.swift
+//  ADUtils
+//
+//  Created by Gaétan Zanella on 18/11/21.
+//
+//
+
+import Foundation
+import Quick
+import Nimble
+import ADUtils
+
+@available(iOS 13.0, *)
+@available(tvOSApplicationExtension 13.0, *)
+class UIDirectionalRectEdgeTest: QuickSpec {
+
+    override func spec() {
+
+        describe("horizontal edges") {
+            let horizontalEdge: UIDirectionalRectEdge = .horizontal
+            it("should contain horizontal values") {
+                expect(horizontalEdge).to(contain(.trailing))
+                expect(horizontalEdge).to(contain(.leading))
+            }
+
+            it("should not contain vertical values") {
+                expect(horizontalEdge).toNot(contain(.bottom))
+                expect(horizontalEdge).toNot(contain(.top))
+            }
+        }
+
+        describe("vertical edges") {
+            let verticalEdge: UIDirectionalRectEdge = .vertical
+            it("should contain vertical values") {
+                expect(verticalEdge).to(contain(.bottom))
+                expect(verticalEdge).to(contain(.top))
+            }
+
+            it("should not contain horizontal values") {
+                expect(verticalEdge).toNot(contain(.trailing))
+                expect(verticalEdge).toNot(contain(.leading))
+            }
+        }
+    }
+
+}
