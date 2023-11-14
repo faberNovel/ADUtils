e24083ddc095cce44b97acd9faf0f69feb254716
Alexandre Podlewski <alexandre.podlewski@fabernovel.com>
2023-11-14 15:21:29 +0100

    Create extension on String to get an AttributedString with different formats

--- ADUtilsTests/AttributedStringTest.swift
+++ ADUtilsTests/AttributedStringTest.swift
@@ -6,7 +6,8 @@
 //
 //
 
-import Foundation
+import UIKit
+import SwiftUI
 import Nimble
 import SnapshotTesting
 import Quick
@@ -54,7 +55,7 @@ class AttributedStringTest: QuickSpec {
             assertSnapshot(matching: label, as: .image, named: imageName)
         }
 
-        it("Snapshots should match") {
+        it("NSAttributedString snapshots should match") {
             let format = "Test %1$@ %2$@"
             let value1 = "Toto"
             let value2 = "Testi testo"
@@ -66,6 +67,81 @@ class AttributedStringTest: QuickSpec {
             stringTest(singleFormat, [value1], "SingleFormat")
             let formatWithEmoji = "Test 🦁 %1$@ %2$@"
             stringTest(formatWithEmoji, [value1, value2], "EmojiInFormat")
+        }
+
+        if #available(iOS 15.0, *) {
+            testAttributedString()
+        }
+    }
+
+    @available(iOS 15.0, tvOS 15.0, *)
+    class func testAttributedString() {
+
+        func assertAttributedStringSnapshot(_ attributedString: AttributedString,
+                                            _ imageName: String,
+                                            file: StaticString = #file,
+                                            line: UInt = #line) {
+            let label = Text(attributedString).fixedSize()
+            assertSnapshot(matching: label, as: .image, named: imageName, file: file, line: line)
+        }
+
+        guard
+            let smallFont = UIFont(name: "HelveticaNeue", size: 12.0),
+            let bigFont = UIFont(name: "HelveticaNeue", size: 24.0) else {
+            fail("Font HelveticaNeue do not exists")
+            return
+        }
+
+        var attributes = AttributeContainer()
+        attributes.foregroundColor = .red
+        attributes.font = smallFont
+        var attributes1 = AttributeContainer()
+        attributes1.foregroundColor = .green
+        attributes1.font = smallFont
+        var attributes2 = AttributeContainer()
+        attributes2.foregroundColor = .blue
+        attributes2.font = bigFont
+
+        let arguments = ["11", "22", "33", "44"]
+        let alternatingFormatAttributes = [attributes1, attributes2, attributes1, attributes2]
+
+        it("AttributedString  snapshot should match") {
+            let attributedString1 = "%@-%@-%@-%@".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: alternatingFormatAttributes
+            )
+            assertAttributedStringSnapshot(attributedString1, "differentAttributes")
+            let attributedString2 = "%@-%@-%@-%@".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: [attributes1, attributes1, attributes1, attributes1]
+            )
+            assertAttributedStringSnapshot(attributedString2, "sameAttributes")
+            let attributedString3 = "%1$@-%2$@-%3$@-%4$@".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: alternatingFormatAttributes
+            )
+            assertAttributedStringSnapshot(attributedString3, "explicitIndexInOrder")
+            let attributedString4 = "%4$@-%3$@-%2$@-%1$@".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: alternatingFormatAttributes
+            )
+            assertAttributedStringSnapshot(attributedString4, "explicitIndexReversed")
+            let attributedString5 = "%2$@-%1$@-%@-%@".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: alternatingFormatAttributes
+            )
+            assertAttributedStringSnapshot(attributedString5, "mixedImplicitExplicitOrder")
+            let attributedString6 = "begin 🦁%1$@🦁-🦁%2$@🦁-🦁%3$@🦁-🦁%4$@🦁 end".attributedString(
+                arguments: arguments,
+                defaultAttributes: attributes,
+                differentFormatAttributes: alternatingFormatAttributes
+            )
+            assertAttributedStringSnapshot(attributedString6, "string with emojis")
         }
     }
 }
--- Modules/ADUtils/String+AttributedFormat.swift
+++ Modules/ADUtils/String+AttributedFormat.swift
@@ -102,3 +102,184 @@ extension String {
         return parameterIndex - 1
     }
 }
+
+@available(iOS 15, tvOS 15.0, *)
+extension String {
+
+    /**
+     Create an AttributedString using self as format, and apply same attributes for each argument
+
+     - parameter arguments: Arguments that match format (self)
+
+     - parameter defaultAttributes: Attributes to apply to whole string by default
+
+     - parameter formatAttributes: Attributes to apply for each argument
+
+     - returns: AttributedString with same attributes for each argument
+     */
+    public func attributedString(arguments: [String],
+                                 defaultAttributes: AttributeContainer,
+                                 formatAttributes: AttributeContainer) -> AttributedString {
+        return attributedString(
+            arguments: arguments,
+            defaultAttributes: defaultAttributes,
+            differentFormatAttributes: arguments.map { _ in return formatAttributes }
+        )
+    }
+
+    /**
+     Create an AttributedString using self as format, and apply different attributes for each argument
+
+     `differentFormatAttributes[i]` is applied to `arguments[i]`
+
+     - parameter arguments: Arguments that match format (self)
+
+     - parameter defaultAttributes: Attributes to apply to whole string by default
+
+     - parameter differentFormatAttributes: Attributes to apply for each argument
+
+     - returns: AttributedString with differents attributes for each argument
+     */
+    public func attributedString(
+        arguments: [String],
+        defaultAttributes: AttributeContainer,
+        differentFormatAttributes: [AttributeContainer]
+    ) -> AttributedString {
+        guard arguments.count == differentFormatAttributes.count else {
+            assertionFailure(
+                "Invalid number of format attributes given, got \(arguments.count) argument "
+                + "and \(differentFormatAttributes) format arguments"
+            )
+            return AttributedString()
+        }
+        if #available(iOS 16.0, tvOS 16.0, *) {
+            return attributedStringUsingRegex(
+                arguments: arguments,
+                defaultAttributes: defaultAttributes,
+                differentFormatAttributes: differentFormatAttributes
+            )
+        } else {
+            return attributedStringUsingNSRegularExpression(
+                arguments: arguments,
+                defaultAttributes: defaultAttributes,
+                differentFormatAttributes: differentFormatAttributes
+            )
+        }
+    }
+
+}
+
+@available(iOS 15, tvOS 15.0, *)
+extension String {
+
+    // MARK: - Internal
+
+    internal func attributedStringUsingNSRegularExpression(
+        arguments: [String],
+        defaultAttributes: AttributeContainer,
+        differentFormatAttributes: [AttributeContainer]
+    ) -> AttributedString {
+        do {
+            let nsString = self as NSString
+            var attributedString = AttributedString()
+            var lastIndex = 0
+            var previousImplicitParameterIndex: Int?
+            try patternMatches().forEach { match in
+                attributedString.append(
+                    AttributedString(
+                        nsString.substring(with: NSRange(lastIndex..<match.range.lowerBound)),
+                        attributes: defaultAttributes
+                    )
+                )
+                let matchIndex = self.parameterIndex(
+                    for: match,
+                    previousImplicitParameterIndex: &previousImplicitParameterIndex
+                )
+                let argument = arguments[matchIndex]
+                let format = differentFormatAttributes[matchIndex]
+
+                let attributedArgument = AttributedString(argument, attributes: format)
+                attributedString.append(attributedArgument)
+                lastIndex = match.range.upperBound
+            }
+            attributedString.append(
+                AttributedString(
+                    nsString.substring(with: NSRange(lastIndex..<nsString.length)),
+                    attributes: defaultAttributes
+                )
+            )
+            return attributedString
+        } catch {
+            return AttributedString()
+        }
+    }
+
+    // MARK: - Private
+
+    private func parameterIndex(for patternMatch: NSTextCheckingResult,
+                                previousImplicitParameterIndex: inout Int?) -> Int {
+        let matchRange = patternMatch.range(at: 2)
+        guard matchRange.location != NSNotFound && matchRange.length > 0,
+              let parameterString = .some((self as NSString).substring(with: matchRange)),
+              let parameterIndex = Int(parameterString)
+        else {
+            let parameterIndex = previousImplicitParameterIndex.map { $0 + 1 } ?? 0
+            previousImplicitParameterIndex = parameterIndex
+            return parameterIndex
+        }
+        return parameterIndex - 1
+    }
+}
+
+@available(iOS 16.0, tvOS 16.0, *)
+extension String {
+    @available(iOS 16.0, tvOS 16.0, *)
+    internal func attributedStringUsingRegex(
+        arguments: [String],
+        defaultAttributes: AttributeContainer,
+        differentFormatAttributes: [AttributeContainer]
+    ) -> AttributedString {
+        var attributedString = AttributedString()
+        var lastIndex = self.startIndex
+        var previousImplicitParameterIndex: Int?
+        // swiftlint:disable:next operator_usage_whitespace
+        self.matches(of: /%(?:([1-9][0-9]*)\$)?@/).forEach { (match) in
+            attributedString.append(
+                AttributedString(
+                    self[lastIndex..<match.range.lowerBound],
+                    attributes: defaultAttributes
+                )
+            )
+            let matchIndex = self.parameterIndex(
+                for: match.output,
+                previousImplicitParameterIndex: &previousImplicitParameterIndex
+            )
+            let argument = arguments[matchIndex]
+            let format = differentFormatAttributes[matchIndex]
+            let attributedArgument = AttributedString(argument, attributes: format)
+            attributedString.append(attributedArgument)
+            lastIndex = match.range.upperBound
+        }
+        attributedString.append(
+            AttributedString(self[lastIndex..<self.endIndex], attributes: defaultAttributes)
+        )
+        return attributedString
+    }
+
+    // MARK: - Private
+
+    private func parameterIndex(
+        for patternMatch: (Substring, Substring?),
+        previousImplicitParameterIndex: inout Int?
+    ) -> Int {
+        guard
+            let stringNumber = patternMatch.1,
+            let explicitIndex = Int(stringNumber)
+        else {
+            let parameterIndex = previousImplicitParameterIndex.map { $0 + 1 } ?? 0
+            previousImplicitParameterIndex = parameterIndex
+            return parameterIndex
+        }
+        return explicitIndex - 1
+    }
+}
